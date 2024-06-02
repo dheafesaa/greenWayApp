4efdcb01b3dd14028ccdeada4771a80849051de0
Dhea Fesa Athallah <dhea.athallah@ist.id>
2024-06-02 20:37:36 +0700

    responsive-design fix wording at campaign list

--- src/pages/CampaignPage.jsx
+++ src/pages/CampaignPage.jsx
@@ -4,10 +4,12 @@ import { Box, useTheme, useMediaQuery } from '@mui/material';
 import { asyncReceiveCampaigns } from '../states/campaigns/action';
 import CampaignCardList from '../components/organisms/CampaignCardList';
 import Title from '../components/atoms/Title';
+import Subtitle from '../components/atoms/Subtitle';
 
 function CampaignPage() {
   const theme = useTheme();
   const isMobileOrTablet = useMediaQuery(theme.breakpoints.down('md'));
+  const isTabletOrDesktop = useMediaQuery(theme.breakpoints.up('sm'));
 
   const dispatch = useDispatch();
   const campaigns = useSelector((state) => state?.campaigns || []);
@@ -18,7 +20,24 @@ function CampaignPage() {
 
   return (
     <Box py={6} px={isMobileOrTablet ? 4 : 10}>
-      <Title title="Campaign" />
+      <Title title={(
+        <>
+          Join the Campaign for Protecting Nature,
+          {' '}
+          {isTabletOrDesktop && <br />}
+          Preserving Beauty
+        </>
+      )}
+      />
+      <Subtitle subtitle={(
+        <>
+          Join our efforts to safeguard the natural wonders of Indonesia.
+          {' '}
+          {isTabletOrDesktop && <br />}
+          Explore our environmental initiatives and be part of the change for a sustainable future.
+        </>
+      )}
+      />
       <CampaignCardList campaignCards={campaigns} />
     </Box>
   );
