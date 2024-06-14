a92d64fca6eea503faf09a3c54eda1ce3dbb3b01
Dhea Fesa Athallah <dhea.athallah@ist.id>
2024-06-14 22:18:20 +0700

    responsive-design fix cardVideo component

--- src/components/atoms/CardVideo.jsx
+++ src/components/atoms/CardVideo.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 
 function CardVideo({ src, alt }) {
   return (
-    <Card
-      component="video"
-      autoPlay
-      loop
-      controls
-      src={src}
-      alt={alt}
-      sx={{
-        backgroundColor: 'transparent',
-        boxShadow: 'none',
-        width: '100%',
-        height: '100%',
-        borderRadius: '16px',
-      }}
-    />
+    <Box sx={{ flexGrow: 1, py: 4 }}>
+      <Card
+        component="video"
+        autoPlay
+        loop
+        controls
+        src={src}
+        alt={alt}
+        sx={{
+          width: '100%',
+          height: '100%',
+          borderRadius: '16px',
+        }}
+      />
+    </Box>
   );
 }
 
