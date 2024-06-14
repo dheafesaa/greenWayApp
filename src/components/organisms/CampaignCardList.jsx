b13b6ec62b7055cd411659388d02ec17e8dae70f
Dhea Fesa Athallah <dhea.athallah@ist.id>
2024-06-14 21:40:34 +0700

    responsive-design fix campaign list

--- src/components/organisms/CampaignCardList.jsx
+++ src/components/organisms/CampaignCardList.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Box, Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import { IoIosArrowForward } from 'react-icons/io';
 import CampaignCardItem, { campaignCardItemShape } from '../molecules/CampaignCardItem';
 
 function CampaignCardList({ campaignCards }) {
   return (
     <Box sx={{ flexGrow: 1, py: 4 }}>
+      <Box display="flex" justifyContent="end" mb={2}>
+        <Button
+          component={Link}
+          to="/campaigns"
+          variant="contained"
+          size="small"
+          endIcon={<IoIosArrowForward />}
+        >
+          See All
+        </Button>
+      </Box>
       <Grid container spacing={4}>
         {campaignCards.map((campaignCard) => (
           <Grid item xs={12} sm={6} md={3} key={campaignCard.id}>
